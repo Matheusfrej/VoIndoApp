f753fbb3cc5609902579718d9898878ffe87d74f
Matheus Frej <matheusfrej@hotmail.com>
2023-03-19 19:20:49 -0300

    Criação do projeto

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,9 @@
+import { HomeContainer, Text } from './styles'
+
+export function Home() {
+  return (
+    <HomeContainer>
+      <Text>Home</Text>
+    </HomeContainer>
+  )
+}
---
+++ src/screens/Home/styles.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components/native'
+
+export const HomeContainer = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+  background-color: ${props => props.theme.color.PRIMARY};
+`
+
+export const Text = styled.Text`
+  font-size: 48px;
+  font-weight: 600;
+  font-family: "InterRegular";
+`
