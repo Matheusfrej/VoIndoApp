06e024f722475f68bae010b791aa633fad2a23ea
RodrigoMesel <rmlo@cin.ufpe.br>
2023-03-22 14:21:37 -0300

    (VN-40) AvaliationCard component created

---
+++ src/components/AvaliationCard/index.tsx
@@ -0,0 +1,44 @@
+import { Image } from 'react-native'
+import { CardContainer, Top, Perfil, Stars } from '../AvaliationCard/styles'
+
+import { CustomText } from '../CustomText'
+
+export function AvaliationCard() {
+  const goToProfile = () => {
+    console.log('Apertou')
+  }
+  return (
+    <CardContainer>
+      <Top>
+        <CustomText type="span"> Rodrigo </CustomText>
+        <Perfil onPress={goToProfile}> Ver Perfil </Perfil>
+      </Top>
+
+      <Stars>
+        <Image
+          source={require('../../../assets/Star.png')}
+          alt=""
+          style={{ marginRight: 2 }}
+        />
+        <Image
+          source={require('../../../assets/Star.png')}
+          alt=""
+          style={{ marginRight: 2 }}
+        />
+        <Image
+          source={require('../../../assets/Star.png')}
+          alt=""
+          style={{ marginRight: 2 }}
+        />
+        <Image
+          source={require('../../../assets/Star.png')}
+          alt=""
+          style={{ marginRight: 2 }}
+        />
+        <Image source={require('../../../assets/Star.png')} alt="" />
+      </Stars>
+
+      <CustomText type="body"> Gostei muito da atividade</CustomText>
+    </CardContainer>
+  )
+}
---
+++ src/components/AvaliationCard/styles.ts
@@ -0,0 +1,27 @@
+import styled from 'styled-components/native'
+
+export const CardContainer = styled.View`
+  width: 80%;
+  padding: 16px;
+  border: 1px solid black;
+  border-radius: 4px;
+  gap: 35px;
+  justify-content: space-between;
+`
+
+export const Top = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+`
+
+export const Perfil = styled.Text`
+  border: none;
+  text-decoration: underline;
+  font-size: 15px;
+`
+
+export const Stars = styled.View`
+  flex: 1;
+  align-items: center;
+  flex-direction: row;
+`
